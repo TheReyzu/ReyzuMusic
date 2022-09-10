b0511b93188e9e8dd1c205242a4c559b3d002725
Reyzu <reyzuproject@gmail.com>
2022-09-10 15:55:15 +0700

    Update dev.py

--- ReyzuMusic/plugins/devs/dev.py
+++ ReyzuMusic/plugins/devs/dev.py
@@ -1,16 +1,3 @@
-#
-# Copyright (C) 2022-2023 by Reyzuuu@Github, < https://github.com/Reyzuuu >.
-#
-# This file is part of < https://github.com/Reyzuuu/ReyzuMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/Reyzuuu/ReyzuMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-
-# This aeval and sh module is taken from < https://github.com/TheHamkerCat/WilliamButcherBot >
-# Credit goes to TheHamkerCat.
-#
-
 import os
 import re
 import subprocess
