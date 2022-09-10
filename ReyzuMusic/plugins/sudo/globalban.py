af11ee46179af2b91561dd5ff4729412c905fe55
Reyzu <reyzuproject@gmail.com>
2022-09-10 15:45:47 +0700

    Update globalban.py

--- ReyzuMusic/plugins/sudo/globalban.py
+++ ReyzuMusic/plugins/sudo/globalban.py
@@ -1,11 +1,3 @@
-#
-# Copyright (C) 2022-2023 by Reyzuuu@Github, < https://github.com/Reyzuuu >.
-#
-# This file is part of < https://github.com/Reyzuuu/ReyzuMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/Reyzuuu/ReyzuMusic/blob/master/LICENSE >
-#
-# All rights reserved.
 import asyncio
 import time
 
