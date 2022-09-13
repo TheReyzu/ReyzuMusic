98b0a968871675a953e33bf72d3aab70f25ea5bf
Reyzu <reyzuproject@gmail.com>
2022-09-14 05:57:19 +0700

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -28,14 +28,14 @@ for filename in os.listdir(r"./strings"):
 
 
 for filename in os.listdir(r"./strings/langs/"):
-    if "en" not in languages:
-        languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
+    if "id" not in languages:
+        languages["id"] = yaml.safe_load(
+            open(r"./strings/langs/id.yml", encoding="utf8")
         )
-        languages_present["en"] = languages["en"]["name"]
+        languages_present["id"] = languages["id"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "en":
+        if language_name == "id":
             continue
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
