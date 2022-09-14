2f7e529e8e1f3de34c95e3d1d1016116aa1968e0
Reyzu <reyzuproject@gmail.com>
2022-09-14 16:02:44 +0700

    Update help.py

--- ReyzuMusic/plugins/bot/help.py
+++ ReyzuMusic/plugins/bot/help.py
@@ -4,6 +4,7 @@ from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
 import config
+from config.config import OWNER_ID
 from config import BANNED_USERS
 from strings import get_command, get_string, helpers
 from ReyzuMusic import app
