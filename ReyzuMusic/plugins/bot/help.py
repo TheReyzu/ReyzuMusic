00d216564f0e5b2ac44ddb9a4f04c80d1f6be603
Reyzu <reyzuproject@gmail.com>
2022-09-14 16:25:02 +0700

    Update help.py

--- ReyzuMusic/plugins/bot/help.py
+++ ReyzuMusic/plugins/bot/help.py
@@ -1,9 +1,5 @@
 from typing import Union
 
-from pyrogram import Client
-from pyrogram.errors import (ChatAdminRequired,
-                             UserAlreadyParticipant,
-                             UserNotParticipant)
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
