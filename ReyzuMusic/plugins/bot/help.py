5c349a15171761090a1beed954d0f0f1e3a48291
Reyzu <reyzuproject@gmail.com>
2022-09-14 14:55:31 +0700

    Update help.py

--- ReyzuMusic/plugins/bot/help.py
+++ ReyzuMusic/plugins/bot/help.py
@@ -45,11 +45,11 @@ async def helper_private(
         if update.message.photo:
             await update.message.delete()
             await update.message.reply_text(
-                _["start_2"].format(config.MUSIC_BOT_NAME), reply_markup=keyboard
+                _["help_1"].format(config.MUSIC_BOT_NAME), reply_markup=keyboard
             )
         else:
             await update.edit_message_text(
-                _["start_2"].format(config.MUSIC_BOT_NAME), reply_markup=keyboard
+                _["help_1"].format(config.MUSIC_BOT_NAME), reply_markup=keyboard
             )
     else:
         chat_id = update.chat.id
@@ -61,7 +61,7 @@ async def helper_private(
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = help_pannel(_)
-        await update.reply_text(_["start_2"].format(config.MUSIC_BOT_NAME), reply_markup=keyboard)
+        await update.reply_text(_["help_1"].format(config.MUSIC_BOT_NAME), reply_markup=keyboard)
 
 
 @app.on_message(
